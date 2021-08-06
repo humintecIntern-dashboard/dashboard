22da6670ed0790dcf1e0898b942b0dad1d3f5041
kimkihoon <63439911+kimkihoon0515@users.noreply.github.com>
2021-08-06 09:13:57 +0900

    Revert "vuetify simple table추가"

--- dashboard-vue/src/main.js
+++ dashboard-vue/src/main.js
@@ -4,14 +4,12 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import './plugins/axios'
-import vuetify from './plugins/vuetify'
 
 
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
-  vuetify,
   el: '#app',
   router,
   components: { App },
